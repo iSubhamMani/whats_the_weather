2efa34872df8caf50ed6495ce258a6114cad04d2
Subham Mani <manisubham09@gmail.com>
2023-10-01 15:48:54 +0530

    get user search input

---
+++ src/search_form.js
@@ -0,0 +1,18 @@
+import fetchCurrentWeather from "./fetch_current_weather";
+import Weather from "./weather";
+
+export default function setUpSearchForm(){
+    const submitBtn = document.querySelector('.search-btn');
+    const searchField = document.getElementById('search-field');
+
+    submitBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const searchQuery = searchField.value;
+
+        const response = await fetchCurrentWeather(`${searchQuery}`);
+        if(response){
+            const data = new Weather(response);
+            console.log(data);
+        }
+    })
+}
